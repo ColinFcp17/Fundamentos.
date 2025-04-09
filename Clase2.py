56436a064d5cafc75e7a46c29b81cca840838425
FranciscoP <fr.pueblap@duocuc.cl>
2025-04-09 09:41:23 -0400

    Add files via upload
    Tarea clase 2 Phyton 
    
    Signed-off-by: FranciscoP <fr.pueblap@duocuc.cl>

---
+++ Clase2.py
@@ -0,0 +1,47 @@
+#Definir Suma
+V01 = int(input("Dime tu numero entero para suma: "))
+V02 = float(input("Dime tu numero decimal para suma: "))
+
+#Operacion
+
+R01 = V01 + V02 
+
+#Salida
+
+print(f"El resultado es: {R01}")
+
+#Definir Resta
+V01 = int(input("Dime tu numero entero para resta: "))
+V02 = float(input("Dime tu numero decimal para resta: "))
+
+#Operacion
+
+R01 = V01 - V02 
+
+#Salida
+
+print(f"El resultado es: {R01}")
+
+#Definir Multiplicacion
+V01 = int(input("Dime tu numero entero para multiplicacion: "))
+V02 = float(input("Dime tu numero decimal para multiplicacion: "))
+
+#Operacion
+
+R01 = V01 * V02 
+
+#Salida
+
+print(f"El resultado es: {R01}")
+
+#Definir Division
+V01 = int(input("Dime tu numero entero para division: "))
+V02 = float(input("Dime tu numero decimal para division: "))
+
+#Operacion
+
+R01 = V01 / V02 
+
+#Salida
+
+print(f"El resultado es: {R01}")
