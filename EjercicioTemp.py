cddd6069041c3276ca6122ac076fe9e9c6760af5
FranciscoP <fr.pueblap@duocuc.cl>
2025-05-30 09:32:07 -0400

    Add files via upload
    Signed-off-by: FranciscoP <fr.pueblap@duocuc.cl>

---
+++ EjercicioTemp.py
@@ -0,0 +1,20 @@
+import time
+
+print('Buen dia')
+
+while True:
+    try:
+        N_temperatura = int(input('Ingrese la temperatura actual: '))
+        if N_temperatura >= -50 and N_temperatura <= 50:
+            print('Temperatura registrada existosamente')
+            break
+        else:
+            print('Error: temperatura fuera del rango, intente nuevamente...')
+    except ValueError:
+        print('ERROR... debe ingresar un número entero valido. Intente hasta que sea valido')
+
+
+time.sleep(5)
+print(f'La temperatura es de {N_temperatura}°c')
+time.sleep(3)
+print('Cierre del programa, !Hasta luego¡')
